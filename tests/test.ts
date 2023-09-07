f128eea015d39461625d55f3f449f35900f0ca9d
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-09-07 17:44:05 -0400

    ci: publish

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected container headings', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Container A' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Container B' })).toBeVisible();
 });
